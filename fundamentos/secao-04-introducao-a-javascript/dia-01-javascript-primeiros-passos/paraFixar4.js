361479bb5a43c68d8f997b11cd3e2ed7b9b433f9
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-25 18:59:23 -0300

    Exercicios paraFixar!

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/paraFixar4.js
@@ -0,0 +1,9 @@
+const conditionOne = true;
+const conditionTwo = false;
+
+console.log(conditionOne && conditionTwo);
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/paraFixar6.js
@@ -0,0 +1,7 @@
+let weekDay = 'Quarta-Feira'
+
+if (weekDay === 'Segunda-Feira' || weekDay === 'Terça-Feira' || weekDay === 'Quarta-Feira' || weekDay === 'Quinta-Feira' || weekDay === 'Sexta-Feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe :)')
+} else {
+    console.log('FINALMENTE, descanso merecido UwU')
+}
