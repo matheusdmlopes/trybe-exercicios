5e1b5b0756656c5d2fcb9f9f8dea06e0fcd6769d
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-25 16:45:02 -0300

    fiz exercicio para fixar.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/paraFixar1.js
@@ -0,0 +1,9 @@
+const myName = "Matheus"
+const birthCity = "São Paulo"
+let birthYear = 2003
+birthYear = 2030
+
+console.log(myName,birthCity,birthYear)
+
+birthCity = "Paraná"
+console.log(birthCity)
