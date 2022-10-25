a1f6fe4210b776693d20c261a7c7f492f40786b6
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-25 18:34:53 -0300

    Exercicio paraFixar3 feito!

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/paraFixar3.js
@@ -0,0 +1,9 @@
+const notaPessoa = 59
+
+if (notaPessoa >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!')
+} else if (notaPessoa < 80 && notaPessoa >= 60) {
+    console.log('Você está na nossa lista de espera :)')
+} else {
+    console.log('Você foi reprovado :(')
+}
