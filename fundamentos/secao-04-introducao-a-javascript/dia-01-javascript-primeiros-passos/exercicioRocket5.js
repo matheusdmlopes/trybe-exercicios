2bf5feba9e1e42c5e1f6313a78bf709b3c127d2e
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-27 13:27:53 -0300

    Primeira parte da alliquota de inss do exercicio feito, com ajuda do gabarito.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicioRocket5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicioRocket5.js
@@ -2,3 +2,15 @@ let aliquotaInss;
 let aliquotaIr; 
 
 const salarioBruto = 3600
+
+if (salarioBruto <= 1556.94) {
+    aliquotaInss = salarioBruto * 0.08
+} else if (salarioBruto <=2594.92) {
+    aliquotaInss = salarioBruto * 0.09
+} else if (salarioBruto <= 5189.82) {
+    aliquotaInss = salarioBruto * 0.11
+} else {
+    aliquotaInss = 570.88
+}
+
+let salarioDescontadoInss = salarioBruto - aliquotaInss
