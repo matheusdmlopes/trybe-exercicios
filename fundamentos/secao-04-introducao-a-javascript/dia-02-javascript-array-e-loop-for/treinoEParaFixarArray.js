d3f47815337d8e651156ff154cf08d362c7b9411
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-27 14:19:11 -0300

    Fiz o para fixar de array

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/treinoEParaFixarArray.js
@@ -0,0 +1,31 @@
+// let listaTarefas = ['Acordar', 'Comer', 'Dormir']
+
+// console.log(listaTarefas.length)
+
+// let buscandoPrimeiraTarefa = listaTarefas[0]
+
+// console.log(buscandoPrimeiraTarefa)
+
+
+
+// listaTarefas.push('Estudar na Trybe')
+// let buscandoUltimaTarefa = listaTarefas[listaTarefas.length - 1]
+
+// console.log(buscandoUltimaTarefa)
+
+// EXERCÍCIO PARA FIXAR.
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1] ;
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
