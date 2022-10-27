e8af99730b0cde4119cf848c3494a5a46c48ca7c
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-27 14:46:23 -0300

    Fiz o para fixar de for.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/treinoEParaFixarFor.js
@@ -0,0 +1,11 @@
+// let cars = ['Saab', 'Volvo', 'BMW','Carro1','Carro2','Carro3'];
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let indice = 0; indice < groceryList.length; indice += 1) {
+    console.log(groceryList[indice]);
+}
