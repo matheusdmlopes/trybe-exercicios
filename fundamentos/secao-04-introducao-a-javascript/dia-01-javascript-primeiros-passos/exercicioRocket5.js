4c9cb347a430760c8c75609309f67980f2af9f5f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-27 13:33:10 -0300

    Finalizei exercicio sobre salario, inss e IR. Muito bom esse exercicio, bem dificil tambem.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicioRocket5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicioRocket5.js
@@ -1,7 +1,7 @@
 let aliquotaInss; 
 let aliquotaIr; 
 
-const salarioBruto = 3600
+const salarioBruto = 3000
 
 if (salarioBruto <= 1556.94) {
     aliquotaInss = salarioBruto * 0.08
@@ -14,3 +14,19 @@ if (salarioBruto <= 1556.94) {
 }
 
 let salarioDescontadoInss = salarioBruto - aliquotaInss
+
+if (salarioDescontadoInss <= 1903.98) {
+    aliquotaIr = 0
+} else if (salarioDescontadoInss <= 2826.65) {
+    aliquotaIr = (salarioDescontadoInss * 0.075) - 142.80
+} else if (salarioDescontadoInss <= 3751.05) {
+    aliquotaIr = (salarioDescontadoInss * 0.15) - 354.80
+} else if (salarioDescontadoInss <= 4664.68) {
+    aliquotaIr = (salarioDescontadoInss * 0.225) - 636.13
+} else {
+    aliquotaIr = (salarioDescontadoInss * 0.275) - 869.36
+}
+
+let salarioFinal = salarioDescontadoInss - aliquotaIr
+
+console.log(salarioFinal)
