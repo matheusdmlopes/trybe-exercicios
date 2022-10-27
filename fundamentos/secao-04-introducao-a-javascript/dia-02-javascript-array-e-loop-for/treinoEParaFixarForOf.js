a02c6eb0964235aef11b50ed28bec463ff8d98fd
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-10-27 15:05:44 -0300

    Terminei o for/of.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/treinoEParaFixarForOf.js
@@ -0,0 +1,26 @@
+let numeros = [3, 6, 9, 12, 15, 18]
+
+for (let numero of numeros) {
+    console.log(numero)
+}
+
+let palavra = 'Bom dia'
+
+for (let word of palavra) {
+    console.log(word)
+}
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+// 11
+// 21
+// 31
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let index of names) {
+    console.log(index)
+}
