1fea0e661bd4edef61cd6622c4a0e50b871b4b27
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-03 18:05:45 -0300

    Fiz a terceira tarefa, da media aritmetica.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exerciciosArrayELoopFor.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exerciciosArrayELoopFor.js
@@ -3,10 +3,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0
 
 for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index]
+    resultado += numbers[index] / 10
     
 }
 
 console.log(resultado)
-
-
