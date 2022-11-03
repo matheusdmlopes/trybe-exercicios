887b873bf8edf8034178f152f8b26423635743c3
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-03 18:23:21 -0300

    tarefa 5

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exerciciosArrayELoopFor.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exerciciosArrayELoopFor.js
@@ -1,16 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let resultado = 0
+let higherNumber = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index] / numbers.length
-    
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
 }
 
-console.log(resultado)
-
-if (resultado > 20) {
-    console.log('valor maior que 20')
-} else if (resultado < 20) {
-    console.log('valor menor que 20')
-}
+console.log(higherNumber);
