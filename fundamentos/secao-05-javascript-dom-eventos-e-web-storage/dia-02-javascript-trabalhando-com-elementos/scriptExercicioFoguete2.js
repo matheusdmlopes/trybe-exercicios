403b494e7ef57efd6ea8807b5655af80be687383
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-07 18:44:42 -0300

    Em teoria criei um filho pro elementoOndeVoceEsta.

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptExercicioFoguete2.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptExercicioFoguete2.js
@@ -4,3 +4,6 @@ const pai = document.getElementById("pai")
 
 const adicionarIrmao = document.createElement('section')
 pai.appendChild(adicionarIrmao)
+
+const criarFilho = document.createElement('section')
+elementoOndeVoceEsta.appendChild(criarFilho)
