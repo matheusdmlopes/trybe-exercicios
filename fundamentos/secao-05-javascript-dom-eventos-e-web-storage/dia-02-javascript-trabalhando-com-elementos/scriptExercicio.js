30a2ff565030f8095ef331f68916b91fec260f62
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-08 17:46:53 -0300

    Armazenei as informaçoes em constantes e fiz um loop for equivocado.

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptExercicio.js
@@ -0,0 +1,10 @@
+const paiDeTodos = document.getElementById("paiDoPai")
+
+const todosOsFilhos = paiDeTodos.childNodes
+
+
+for (let index = 0; index < paiDeTodos.length; index += 1) {
+    let element = paiDeTodos[index]
+
+    if (element.includes(""))
+}
