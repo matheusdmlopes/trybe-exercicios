abea8b71bd5bed071fdbe0902ac5319063d96b9b
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-08 17:59:14 -0300

    Fiz o exercício como sugerido no gabarito, mas pareceu estar errado lá, entao fiz de outra maneira. Posso estar equivocado.

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptExercicio.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/scriptExercicio.js
@@ -3,8 +3,9 @@ const paiDeTodos = document.getElementById("paiDoPai")
 const todosOsFilhos = paiDeTodos.childNodes
 
 
-for (let index = 0; index < paiDeTodos.length; index += 1) {
-    let element = paiDeTodos[index]
-
-    if (element.includes(""))
+for (let index = 0; index < todosOsFilhos.length; index += 1) {
+    let filhoAtual = todosOsFilhos[index]
+    if (filhoAtual.id !== 'elementoOndeVoceEsta' && filhoAtual.id !== 'pai' && filhoAtual.id !== 'primeroFilhoDoFilho') {
+        filhoAtual.remove();
+    }
 }
