84092d93b625e4357abbfb6217697e9a5fe852b9
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-09 18:58:16 -0300

    to fazendo os exercicios no passo a passo do gabarito

--- fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/scriptExercicioFoguete.js
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/scriptExercicioFoguete.js
@@ -13,4 +13,21 @@ const createDaysOfTheWeek = () => {
 
 createDaysOfTheWeek();
 
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function criarDiasDoMes() {
+    let listaDosDias = document.getElementById('days')
+
+    for (let i = 0; i < decemberDaysList.length; i += 1) {
+
+        let dia = decemberDaysList[i]
+        let diaLi = document.createElement('li')
+        diaLi.innerHTML = dia
+
+        if ()
+    }
+}
+
+
