3006485afd6f5bf58ce23053de48cf8a9e1d9433
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2022-11-14 16:51:44 -0300

    fiz a funçao que armazena o backgroundcolor

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+
+    const setBackgroundColor = (color) => {
+        let conteudo = document.body
+        conteudo.style.backgroundColor = color
+        localStorage.setItem('backgroundColor', color)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
