9b501d0eed889a600fa224ad5b8144efcf69ca5f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-28 12:44:02 -0300

    Finalizei o exercicio 2.

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio2.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio2.test.js
@@ -5,6 +5,15 @@ describe('Testando os bagulhos', () => {
         expect(myFizzBuzz(15)).toBe('fizzbuzz')
     })
     test('Execute a função myFizzBuzz(num), sendo num um número divisível por 3, e verifique se o retorno é o esperado.', () => {
-
+        expect(myFizzBuzz(9)).toBe('fizz')
+    })
+    test('Execute a função myFizzBuzz(num), sendo num um número divisível por 5, e verifique se o retorno é o esperado.', () => {
+        expect(myFizzBuzz(25)).toBe('buzz')
+    })
+    test('Execute a função myFizzBuzz(num), sendo num um número que não é divisível por 3 ou 5, e verifique se o retorno é o esperado.', () => {
+        expect(myFizzBuzz(37)).toBe(37)
+    })
+    test('Execute a função myFizzBuzz(num), sendo num um parâmetro que não é um número, e verifique se o retorno é o esperado.', () => {
+        expect(myFizzBuzz('boa tarde')).toBe(false)
     })
 })
