1fb71c0423c26737e2015e6dc360f464edd2b609
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-29 19:00:59 -0300

    exercicio fodase

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
@@ -4,4 +4,10 @@ describe('testando os bagulhos', () => {
     test('Teste se o retorno de sum(4, 5) é 9;', () => {
         expect(sum(4, 5)).toBe(9)
     })
+    test('Teste se o retorno de sum(0, 0) é 0.', () => {
+        expect(sum(0, 0)).toBe(0)
+    })
+    test('Teste se a função sum lança um erro quando os parâmetros são number 4 e string '5';', () => {
+        expect(sum(4, '5')).toThrow('parameters must be numbers')
+    })
 })
