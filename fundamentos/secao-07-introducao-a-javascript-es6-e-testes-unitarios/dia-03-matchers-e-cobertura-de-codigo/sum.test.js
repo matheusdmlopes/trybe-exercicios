336cb4994c699bcc4bad2204cecc43afb2171ab4
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-29 19:04:02 -0300

    testes feitos

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
@@ -7,7 +7,10 @@ describe('testando os bagulhos', () => {
     test('Teste se o retorno de sum(0, 0) é 0.', () => {
         expect(sum(0, 0)).toBe(0)
     })
-    test('Teste se a função sum lança um erro quando os parâmetros são number 4 e string '5';', () => {
-        expect(sum(4, '5')).toThrow('parameters must be numbers')
+    test('Teste se a função sum lança um erro quando os parâmetros são number 4 e string', () => {
+        expect(sum(4, '5')).toThrow(Error)
+    })
+    test('Teste se a mensagem de erro é parameters must be numbers quando realizar a chamada sum(4, string 5);', () => {
+        expect(sum(4, '5')).toThrow(new Error('parameters must be numbers'))
     })
 })
