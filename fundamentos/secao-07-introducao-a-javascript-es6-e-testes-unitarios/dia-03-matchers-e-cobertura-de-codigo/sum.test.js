fd6fa0739bf030e299520272cc90814ec93c527b
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-29 19:35:24 -0300

    corrigi os toThrowError

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/sum.test.js
@@ -1,4 +1,4 @@
-const sum = requires('./sum.js');
+const sum = require('./sum.js');
 
 describe('testando os bagulhos', () => {
     test('Teste se o retorno de sum(4, 5) é 9;', () => {
@@ -8,9 +8,11 @@ describe('testando os bagulhos', () => {
         expect(sum(0, 0)).toBe(0)
     })
     test('Teste se a função sum lança um erro quando os parâmetros são number 4 e string', () => {
-        expect(sum(4, '5')).toThrow(Error)
+        // expect(sum(4, '5')).toThrowError(Error)
+        expect(() => sum(4, '5')).toThrow(Error)
     })
     test('Teste se a mensagem de erro é parameters must be numbers quando realizar a chamada sum(4, string 5);', () => {
-        expect(sum(4, '5')).toThrow(new Error('parameters must be numbers'))
+        // expect(sum(4, '5')).toThrow(new Error('parameters must be numbers'))
+        expect(() => sum(4, '5')).toThrow(new Error('parameters must be numbers'))
     })
 })
