5ed7ff000b9e7ad9f19d5a5bd9dedf0432a14cb5
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-30 15:59:20 -0300

    fiz com ajuda do gabarito, dei uma bugada no parametro q passo pro find

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar2.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar2.js
@@ -1,6 +1,8 @@
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+
 
 // exercicio 1
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const verifyDiv = numbers.find((idade) => idade % 3 === 0 && idade % 5 === 0)
 
@@ -14,3 +16,15 @@ const verifyTudo = () => {
     return names.find((name) => name.length === 5)
 }
 console.log(verifyTudo());
+
+// exercicio 3
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+const verifyEssaPorra = musicas.find((musica) => musica.id === '31031685')
+
+console.log(verifyEssaPorra);
