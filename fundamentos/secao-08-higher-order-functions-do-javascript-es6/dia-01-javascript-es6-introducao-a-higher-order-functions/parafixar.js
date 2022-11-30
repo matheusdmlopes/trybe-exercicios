8f23d29dc0a11ac9beabb08661d591c7555046b7
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-11-30 10:35:48 -0300

    para fixar feito

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar.js
@@ -0,0 +1,14 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+];
+
+// Adicione seu código aqui
+
+emailListInData.forEach((element, index) => {
+    console.log(`O email ${element} está cadastrado em nosso banco de dados!`)
+    console.log(`Sua posição no array é ${index}`)
+})
+
