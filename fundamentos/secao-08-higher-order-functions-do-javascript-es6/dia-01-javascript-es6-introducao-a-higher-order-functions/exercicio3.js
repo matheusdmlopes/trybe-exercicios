d3d844a44d3749c97ed78fcfeba0741a8c4cd7b8
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-12-01 11:36:38 -0300

    exercicio 3 feito com ajuda do gabarito, mas to pegando a ideia das hofs

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
@@ -1,6 +1,18 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const baseFunctionHOF = (rightAnswers, studentAnswers, action) => {
-
+const verifyAnswers = (rightAnswers, studentAnswers) => {
+    if (studentAnswers === rightAnswers) return 1
+    if (studentAnswers === 'N.A') return 0
+    if (rightAnswers !== studentAnswers) return -0.5
 }
+
+const baseFunctionHOF = (rightAnswers, studentAnswers, action) => {
+    let contador = 0
+    for (let index = 0; index < rightAnswers.length; index += 1) {
+        const compareAnswers = action(rightAnswers[index], studentAnswers[index])
+        contador += compareAnswers
+    }
+    return `Resultado final: ${contador}`
+}
+console.log(baseFunctionHOF(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
