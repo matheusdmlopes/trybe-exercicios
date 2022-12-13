1f4d0b9d62334e622be9cd793be1011a9568c670
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-12-13 18:41:28 -0300

    exercicio 3feito,e irei começar o exercicio 4

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
@@ -1,18 +1,53 @@
+// const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+// const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+// const verifyAnswers = (rightAnswers, studentAnswers) => {
+//     if (studentAnswers === rightAnswers) return 1
+//     if (studentAnswers === 'N.A') return 0
+//     if (rightAnswers !== studentAnswers) return -0.5
+// }
+
+// const baseFunctionHOF = (rightAnswers, studentAnswers, action) => {
+//     let contador = 0
+//     for (let index = 0; index < rightAnswers.length; index += 1) {
+//         const compareAnswers = action(rightAnswers[index], studentAnswers[index])
+//         contador += compareAnswers
+//     }
+//     return `Resultado final: ${contador}`
+// }
+// console.log(baseFunctionHOF(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const verifyAnswers = (rightAnswers, studentAnswers) => {
-    if (studentAnswers === rightAnswers) return 1
+    if (rightAnswers === studentAnswers) return 1
     if (studentAnswers === 'N.A') return 0
-    if (rightAnswers !== studentAnswers) return -0.5
+    if (studentAnswers !== rightAnswers) return -0.5
 }
 
-const baseFunctionHOF = (rightAnswers, studentAnswers, action) => {
-    let contador = 0
-    for (let index = 0; index < rightAnswers.length; index += 1) {
-        const compareAnswers = action(rightAnswers[index], studentAnswers[index])
-        contador += compareAnswers
-    }
-    return `Resultado final: ${contador}`
+const highOrderFunction = (rightAnswers, studentAnswers, callback) => {
+
+    let counter = 0
+
+    rightAnswers.forEach((element, index) => {
+        const compareAnswers = callback(rightAnswers[index], studentAnswers[index])
+        counter += compareAnswers
+    });
+    return `Resultado final: ${counter}`
 }
-console.log(baseFunctionHOF(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
+
+console.log(highOrderFunction(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers))
