ba8b4d7b0d61c9ab23923f1df34e3372df6e5c7b
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-12-13 16:52:53 -0300

    parafixar3 feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar3.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar3.js
@@ -36,7 +36,30 @@ const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
     //Adicione seu código aqui
+    const logica = names.some((element) => element === name)
+    if (logica) return true
+    if (!logica) return false
+
 };
 
 console.log(hasName(names, 'Ana'));
 console.log(hasName(names, 'Pedro'));
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+];
+
+//Adicione seu código aqui
+
+const verifyAges = (arr, idadeMinima) => people.every((element) => {
+    const logicaSla = element.age >= idadeMinima
+    if (logicaSla) return true
+    if (!logicaSla) return false
+})
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
