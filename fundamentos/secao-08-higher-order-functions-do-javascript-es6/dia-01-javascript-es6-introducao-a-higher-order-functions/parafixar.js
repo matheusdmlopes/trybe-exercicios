14b2b2f925cedaf0924c718244fcdd1435f4c008
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-12-13 16:16:46 -0300

    commitando aqui

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar.js
@@ -1,3 +1,17 @@
+// const emailListInData = [
+//     'roberta@email.com',
+//     'paulo@email.com',
+//     'anaroberta@email.com',
+//     'fabiano@email.com',
+// ];
+
+// // Adicione seu código aqui
+
+// emailListInData.forEach((element, index) => {
+//     console.log(`O email ${element} está cadastrado em nosso banco de dados!`)
+//     console.log(`Sua posição no array é ${index}`)
+// })
+
 const emailListInData = [
     'roberta@email.com',
     'paulo@email.com',
@@ -7,8 +21,9 @@ const emailListInData = [
 
 // Adicione seu código aqui
 
-emailListInData.forEach((element, index) => {
-    console.log(`O email ${element} está cadastrado em nosso banco de dados!`)
-    console.log(`Sua posição no array é ${index}`)
+emailListInData.forEach((element) => {
+    console.log
 })
+
+
 
--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/teste.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/teste.js
@@ -1,5 +1,20 @@
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+
+// arrayOfValues.forEach((element, index) => {
+//     console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
+// });
+
 const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
 
 arrayOfValues.forEach((element, index) => {
     console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
 });
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multiply = (element) => console.log(element * 2)
+
+
+// numbers.forEach((element) => console.log(element * 2))
+
+numbers.forEach(multiply)
