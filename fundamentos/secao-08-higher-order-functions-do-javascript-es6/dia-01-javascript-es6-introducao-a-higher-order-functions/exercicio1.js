9c47d1f315ee345b1ada3733428b9c7b4988337a
matheusdmlopes <matheusdemarcolopes@gmail.com>
2022-12-13 17:37:29 -0300

    exercicio1 feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -18,11 +18,18 @@
 
 
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
     const employees = {
-        id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-        id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-        id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+        id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
 };
+
+const logica = (nomeCompleto) => {
+    let mudarFormato = nomeCompleto.toLowerCase().split(' ').join('_')
+    return { nomeCompleto: nomeCompleto, email: `${mudarFormato}@trybe.com ` }
+}
+
+console.log(newEmployees(logica))
