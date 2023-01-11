6de805c127e750113ea191c5efc4f001b6c3734f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-11 16:29:50 -0300

    exericio 5 feito com ajuda do gabarito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exerciciosPraticos.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exerciciosPraticos.js
@@ -120,3 +120,26 @@ const countA = () => {
 }
 
 console.log(countA())
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+// const expectedResult = [
+//  { name: 'Pedro Henrique', average: 7.8 },
+//  { name: 'Miguel', average: 9.2 },
+//  { name: 'Maria Clara', average: 8.8 },
+// ];
+
+const studentAverage = () => {
+    const logic = students.map((element, index) => ({
+        name: element,
+        average: (grades[index].reduce((acc, element) => {
+            return acc + element
+        }, 0)) / grades[index].length
+    }))
+    return logic
+}
+
+console.log(studentAverage())
