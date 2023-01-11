dc9e4694a00b77526f4c936be6b408e1c0404212
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-11 16:12:42 -0300

    exercicio 4 feito com ajuda do gabarito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exerciciosPraticos.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exerciciosPraticos.js
@@ -102,3 +102,21 @@ const longestName = () => {
 }
 
 console.log(longestName());
+
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+// const expectedResult = 20;
+
+const lettersArray = names.join('').toLowerCase().split('')
+
+const countA = () => {
+    return lettersArray.reduce((acc, element) => {
+        return element === 'a' ? acc += 1 : acc
+    }, 0)
+}
+
+console.log(countA())
