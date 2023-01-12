07821f1f1da0e81abeceb69a02e2fcc60c0c184d
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 15:02:10 -0300

    ex4 feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameter-e-object-destructuring/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameter-e-object-destructuring/exercicios.js
@@ -13,3 +13,64 @@ rectangles.forEach((element) => {
 const sum = (...args) => args.reduce((acc, element) => (acc + element), 0)
 
 console.log(sum(4, 5, 6))
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+};
+
+const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+
+// Retornos esperados:
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+const people = [
+    {
+        name: 'Nicole',
+        bornIn: 1992,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Harry',
+        bornIn: 2008,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Toby',
+        bornIn: 1901,
+        nationality: 'Australian',
+    },
+    {
+        name: 'Frida',
+        bornIn: 1960,
+        nationality: 'Dannish',
+    },
+    {
+        name: 'Fernando',
+        bornIn: 2001,
+        nationality: 'Brazilian',
+    },
+    // bornIn: nascido em
+];
+
+const filterPeople = (array) => array.filter(({ nationality, bornIn }) => {
+    return nationality === 'Australian' && bornIn <= 2000
+})
+console.log(filterPeople(people))
+
+  // escreva filterPeople abaixo
