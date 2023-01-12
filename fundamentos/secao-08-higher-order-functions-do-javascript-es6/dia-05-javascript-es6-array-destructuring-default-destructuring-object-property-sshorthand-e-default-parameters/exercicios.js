9424ace3006c46642b0a8694b860c9f1b7725110
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 18:29:46 -0300

    exercicio dos students dentro do array feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
@@ -28,20 +28,31 @@ console.log(greet('John')); // 'Hi John'
 console.log(greet('John', 'Good morning')); // 'Good morning John'
 console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
 
-const student1 = {
-    name: `Claudia`,
-    lastName: `Farias`,
-    age: 23,
-}
+// const student1 = {
+//     name: `Claudia`,
+//     lastName: `Farias`,
+//     age: 23,
+// }
 
-const student2 = {
-    name: `Vitor`,
-    age: 20,
-}
+// const student2 = {
+//     name: `Vitor`,
+//     age: 20,
+// }
 
 // escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
 
 const getLastName = ({ lastName = 'lastName não preenchido' }) => lastName
 
-console.log(getLastName(student1));
-console.log(getLastName(student2));
+// console.log(getLastName(student1));
+// console.log(getLastName(student2));
+
+const moreStudents = [
+    'Chris',
+    ['Ahmad', 'Antigoni'],
+    ['Toby', 'Sam']
+];
+
+// Escreva seu código aqui
+const [student1, [student2, student3], [student4, student5]] = moreStudents;
+
+console.log(student1, student2, student3, student4, student5);
