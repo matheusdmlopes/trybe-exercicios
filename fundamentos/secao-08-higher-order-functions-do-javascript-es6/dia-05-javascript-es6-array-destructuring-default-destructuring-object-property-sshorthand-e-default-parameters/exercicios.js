1ae7b70d18a3862e76463a120cc80acbb7105e2b
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 18:26:34 -0300

    exercicio do last name feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
@@ -27,3 +27,21 @@ const greet = (name, saudação = 'Hi') => `${saudação} ${name}`
 console.log(greet('John')); // 'Hi John'
 console.log(greet('John', 'Good morning')); // 'Good morning John'
 console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+
+const student1 = {
+    name: `Claudia`,
+    lastName: `Farias`,
+    age: 23,
+}
+
+const student2 = {
+    name: `Vitor`,
+    age: 20,
+}
+
+// escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+
+const getLastName = ({ lastName = 'lastName não preenchido' }) => lastName
+
+console.log(getLastName(student1));
+console.log(getLastName(student2));
