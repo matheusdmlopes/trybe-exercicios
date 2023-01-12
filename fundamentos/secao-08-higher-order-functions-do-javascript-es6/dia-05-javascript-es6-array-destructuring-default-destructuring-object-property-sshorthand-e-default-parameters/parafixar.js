25228bf56d2350a694df826b8b606e397eec0a70
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 15:24:14 -0300

    ex2 feito com ajuda do gabarito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
@@ -9,3 +9,13 @@ const sum = ([a, b, c]) => {
 sum(primeNumbers) // 54
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal]
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
