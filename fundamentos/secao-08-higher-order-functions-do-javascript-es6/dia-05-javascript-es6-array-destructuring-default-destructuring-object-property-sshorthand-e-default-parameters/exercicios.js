1ab6bfbe20c54ba2514299d95c99b3a790f7112a
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 18:22:29 -0300

    feito exercicio do greet

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
@@ -8,3 +8,22 @@ const swap2 = ([a, b, c]) => [c, b, a]// jeito certo, esse altera o array;
 
 console.log(swap2(myList))
 console.log(swap(myList))
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+
+const toObject = ([i1, i2, i3]) => ({ i1, i2, i3 })
+
+console.log(toObject(palio))
+
+// escreva greet abaixo
+
+const greet = (name, saudação = 'Hi') => `${saudação} ${name}`
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
