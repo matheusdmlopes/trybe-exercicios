e1a95ea3636ad312c0694492abf570142b2ae69d
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 18:16:27 -0300

    jeito certo agr, o primeiro jeito n muda o array original

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/exercicios.js
@@ -4,4 +4,7 @@ const myList = [5, 2, 3];
 
 const swap = (array) => [array[0], array[1], array[2]] = [array[2], array[1], array[0]]
 
+const swap2 = ([a, b, c]) => [c, b, a]// jeito certo, esse altera o array;
+
+console.log(swap2(myList))
 console.log(swap(myList))
