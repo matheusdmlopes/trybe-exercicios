bfea89091aa3e23dde75bf04d7b1fa4fadb962fd
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 15:20:41 -0300

    outro jeito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
@@ -1,11 +1,11 @@
 const primeNumbers = [17, 23, 37]
 
-const sum = (a, b) => {
-    console.log(a + b);
+const sum = ([a, b, c]) => {
+    console.log(a + c);
 }
 
-const [n1, n2, n3] = primeNumbers
+// const [n1, n2, n3] = primeNumbers
 
-sum(n1, n3) // 54
+sum(primeNumbers) // 54
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
