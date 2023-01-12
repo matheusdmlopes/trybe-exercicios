eeea9459ee919dab641bd72ece7722aebd95feff
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-12 17:43:36 -0300

    parafixar feito

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-sshorthand-e-default-parameters/parafixar.js
@@ -19,3 +19,11 @@ let bebida = 'arroz';
 console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[, , , ...numerosPares] = numerosPares
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
