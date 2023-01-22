c77a5f755ae506b1fd782e5eebf3711abf3edc8a
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-22 13:57:40 -0300

    sla

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/testes/testes.js
@@ -0,0 +1,4 @@
+const array = [{
+    obj1: 'sim',
+    obj2: 2
+}]
---
+++ fundamentos/trybe dinamica/dinamicateste.js
@@ -0,0 +1,26 @@
+const moedas = {
+    success: true,
+    base: "BRL",
+    date: "2023-01-12",
+    rates: {
+        AED: 0.711817,
+        AFN: 17.376908,
+        ALL: 20.974748,
+        AMD: 76.29545,
+        ANG: 0.348689,
+        AOA: 97.619645,
+        ARS: 35.010255,
+        AUD: 0.279934,
+        AWG: 0.348859,
+        AZN: 0.329521,
+        BAM: 0.3521,
+    }
+}
+
+const sla = [...moedas.rates]
+
+console.log(sla)
+
+const getValue = () => {
+
+}
