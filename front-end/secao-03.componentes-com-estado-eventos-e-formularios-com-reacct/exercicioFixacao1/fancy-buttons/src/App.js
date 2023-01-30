d0be5260eac54f13f8c80687661e277e098b6dd1
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-30 12:05:09 -0300

    testando com varios console log

--- front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
+++ front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const imprimirConsole1 = () => {
+  console.log('texto qualquer1')
+}
+const imprimirConsole2 = () => {
+  console.log('texto qualquer2')
+}
+const imprimirConsole3 = () => {
+  console.log('texto qualquer3')
+}
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <button onClick={imprimirConsole1}>botao1</button>
+      <button onClick={imprimirConsole2}>botao1</button>
+      <button onClick={imprimirConsole3}>botao1</button>
     </div>
-  );
+  }
 }
 
 export default App;
