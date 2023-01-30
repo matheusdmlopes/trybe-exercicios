20f2e1b41a344ccfdbbc4f5977e28a271474fa42
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-30 14:50:59 -0300

    testando o this setstate

--- front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
+++ front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
@@ -5,16 +5,34 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
-    console.log('Componente sendo construído')
+    this.state = {
+      numeroDeCliques: 0
+    }
   }
 
-  imprimirConsole() {
-    console.log('clicou')
+  imprimirConsole1 = () => {
+    this.setState({
+      numeroDeCliques: 1
+    })
+    console.log('clicou1')
+
+  }
+  imprimirConsole2 = () => {
+    console.log('clicou2')
+
+  }
+  imprimirConsole3 = () => {
+    console.log('clicou3')
+
   }
 
   render() {
-    console.log(this);
-    return <button onClick={this.imprimirConsole}>botao1</button>
+    console.log(this)
+    return <div>
+      <button onClick={this.imprimirConsole1}>botao1</button>
+      <button onClick={this.imprimirConsole2}>botao2</button>
+      <button onClick={this.imprimirConsole3}>botao3</button>
+   </div>
   }
 }
 
