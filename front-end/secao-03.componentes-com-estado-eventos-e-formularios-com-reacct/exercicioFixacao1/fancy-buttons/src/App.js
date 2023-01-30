e0dfcf197c2567ec7750528e68ac0fa6bced36a7
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-01-30 17:34:58 -0300

    teste com botoes contadores

--- front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
+++ front-end/secao-03.componentes-com-estado-eventos-e-formularios-com-reacct/exercicioFixacao1/fancy-buttons/src/App.js
@@ -6,32 +6,46 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      numeroDeCliques: 0
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
     }
   }
 
   imprimirConsole1 = () => {
     this.setState({
-      numeroDeCliques: 1
+      numeroDeCliques1: this.state.numeroDeCliques1 + 1
     })
     console.log('clicou1')
-
   }
   imprimirConsole2 = () => {
+    this.setState({
+      numeroDeCliques2: this.state.numeroDeCliques2 + 1
+    })
     console.log('clicou2')
-
   }
   imprimirConsole3 = () => {
+    this.setState({
+      numeroDeCliques3: this.state.numeroDeCliques3 + 1
+    })
     console.log('clicou3')
+  }
 
+  limparBotoes = () => {
+    this.setState({
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
+    })
   }
 
   render() {
     console.log(this)
     return <div>
-      <button onClick={this.imprimirConsole1}>botao1</button>
-      <button onClick={this.imprimirConsole2}>botao2</button>
-      <button onClick={this.imprimirConsole3}>botao3</button>
+      <button onClick={this.imprimirConsole1}>{this.state.numeroDeCliques1}</button>
+      <button onClick={this.imprimirConsole2}>{this.state.numeroDeCliques2}</button>
+      <button onClick={this.imprimirConsole3}>{this.state.numeroDeCliques3}</button>
+      <button onClick={this.limparBotoes}>Limpar tudo</button>
    </div>
   }
 }
